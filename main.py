8bfe15aee6636249975df66b935501be8ffb66f2
Vndanilchenko <vndanilchenko@gmail.com>
2021-02-24 18:03:05 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+бот опроса соискателей о причинах отказа от позиции
+
+@ author: vndanilchenko@gmail.com
+"""
+
+
+
+import telebot, os
+
+# импортируем токены
+try:
+    token = os.environ.get('TG_TOKEN')
+except:
+    from private.token import token
+
+bot = telebot.TeleBot(token=token)
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+
+bot.polling()
